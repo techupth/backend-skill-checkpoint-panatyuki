d479c48b1e03d01699de6a34d58456575ec32056
panatyuki <admiral.dancho@gmail.com>
2024-01-16 09:45:01 +0700

    feat: fix a code and add a comments for explain each code

--- app.js
+++ app.js
@@ -11,65 +11,107 @@ async function init() {
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
 
+    // Test the connection point.
+    // ตัวทดสอบสำหรับ Postman ว่าเชื่อมต่อกับ Postman ได้ไหม
     app.get("/", (req, res) => {
-      return res.json("Ready");
+      try {
+        return res.json("Hello Skill Checkpoint #2");
+      } catch (error) {
+        return res.json({
+          Message: `${error}`,
+        });
+      }
     });
 
+    // Start to coding Controller functions here.
+    // 1 ) Controller function สำหรับสร้างคำถามใหม่ (title, description, category)
     app.post("/question", async (req, res) => {
-      const collection = db.collection("questions");
-      const questionData = { ...req.body };
-      const newQuestionData = await collection.insertOne(questionData);
-      return res.json({
-        Message: "new question has been created successfully",
-      });
+      try {
+        const collection = db.collection("questions");
+        const questionData = { ...req.body };
+        const newQuestionData = await collection.insertOne(questionData);
+        return res.json({
+          Message: "new question has been created successfully",
+        });
+      } catch (error) {
+        return res.json({
+          Message: `${error} 401 Can not create a new question. Please recheck your text again.`,
+        });
+      }
     });
 
+    // 2 ) Controller function สำหรับดูคำถามทุกข้อ
     app.get("/questions", async (req, res) => {
-      const collection = db.collection("questions");
-      const allQuestions = await collection.find().toArray();
-      return res.json({
-        data: allQuestions,
-      });
+      try {
+        const collection = db.collection("questions");
+        const allQuestions = await collection.find().toArray();
+        return res.json({ data: allQuestions });
+      } catch (error) {
+        return res.json({
+          Message: `${error} 402 Can not connect the DB. Please try again later.`,
+        });
+      }
     });
 
+    // 3 ) Controller function สำหรับดูคำถามแต่ละข้อแบบระบุ ID
     app.get("/question/:questionId", async (req, res) => {
-      const collection = db.collection("questions");
-      const questionId = new ObjectId(req.params.questionId);
+      try {
+        const collection = db.collection("questions");
+        const questionId = new ObjectId(req.params.questionId);
 
-      const questionById = await collection.findOne({ _id: questionId });
+        const questionById = await collection.findOne({ _id: questionId });
 
-      return res.json({
-        data: questionById,
-      });
+        return res.json({
+          data: questionById,
+        });
+      } catch (error) {
+        return res.json({
+          Message: `${error} 402 Can not connect the DB. Please try again later.`,
+        });
+      }
     });
 
+    // 4 ) Controller function สำหรับแก้ไข อัปเดตตัวคำถาม (หัวข้อ คำอธิบาย หมวดหมู่)
     app.put("/question/:questionId", async (req, res) => {
-      const collection = db.collection("questions");
-      const updateData = { ...req.body };
+      try {
+        const collection = db.collection("questions");
+        const updateData = { ...req.body };
 
-      const questionid = new ObjectId(req.params.questionId);
+        const questionid = new ObjectId(req.params.questionId);
 
-      await collection.updateOne(
-        {
-          _id: questionid,
-        },
-        {
-          $set: updateData,
-        }
-      );
+        await collection.updateOne(
+          {
+            _id: questionid,
+          },
+          {
+            $set: updateData,
+          }
+        );
 
-      return res.json({ Message: "question has been updated successfully" });
+        return res.json({ Message: "question has been updated successfully" });
+      } catch (error) {
+        return res.json({
+          Message: `${error} 403 Please recheck your update text again.`,
+        });
+      }
     });
 
+    // 5 ) Controller function สำหรับลบคำถามออกโดยระบุ ID
     app.delete("/question/:questionId", async (req, res) => {
-      const collection = db.collection("questions");
-      const questionId = new ObjectId(req.params.questionId);
+      try {
+        const collection = db.collection("questions");
+        const questionId = new ObjectId(req.params.questionId);
 
-      await collection.deleteOne({ _id: questionId });
+        await collection.deleteOne({ _id: questionId });
 
-      return res.json({
-        Message: `question ${questionId} has been deleted successfully`,
-      });
+        return res.json({
+          Message: `question ${questionId} has been deleted successfully`,
+        });
+      } catch (error) {
+        return res.json({
+          Message: `${error} 501 Can not connect to the DB. Please try again later.`,
+        });
+      }
     });
 
     app.get("*", (req, res) => {
--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,11 @@
 // Todo: Setup database connection here
-
 // Named Import ตัว MongoClient มาจาก "mongodb"
 import { MongoClient } from "mongodb";
 
 const connectionString = "mongodb://localhost:27017";
 
+// ผมต้องไม่ใส่ useUnifiedTopology: true
+// ถ้าใส่ไปแล้วมันเชื่อมต่อไม่ได้ครับ
 export const client = new MongoClient(connectionString);
 
 export const db = client.db("practice-mongo");
